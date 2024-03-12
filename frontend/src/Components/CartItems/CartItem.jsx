13cd429026304e6bf9575a9efe0ee748283927ab
Sourav <souravmohanta4428@gmail.com>
2024-03-12 12:04:30 +0530

    first commit

--- frontend/src/Components/CartItems/CartItem.jsx
+++ frontend/src/Components/CartItems/CartItem.jsx
@@ -1,43 +1,120 @@
-import React, { useContext ,useState} from "react";
+// import React, { useContext ,useState} from "react";
+// import  './CartItems.css'
+// import { ShopContext } from "../../Context/ShopContext";
+// import remove_icon from '../Assets/cart_cross_icon.png'
+// //import {loadStripe} from '@stripe/stripe-js';//for payment intregation
+// import StripeCheckout from 'react-stripe-checkout'
+// //const apiKey="pk_test_51OpVHpSIyGZ3BZDjKKoAKQ8IZJbLm3xLxkUrMtkuvx4LCCKTcuXeNS42JWaVkgn3RJSJQAlHP8C0x9uD52PODDg500picRPBvt"
+// const apiKey = import.meta.env.VITE_API_KEY;
+// const CartItems= ()=>{
+//     const {getTotalCartAmount,all_product,cartItems,removeFromCart}=useContext(ShopContext);
+//     const [Product,setProduct] = useState({
+//         name:{cartItems},
+//         price:10,
+//         productBy:"facebook"
+//       })
+      
+//     //for payment intregation
+//     const makePayment = token=>{
+//         const body={
+//           token,
+//           Product
+//         }
+//         const headers={
+//           "Content-Type":"application/json"
+//         }
+    
+//         // return fetch(`http://localhost:3000/payment`,{
+//         return fetch(`https://backend3-j9x6.onrender.com/payment`,{
+//           method:"POST",
+//           headers,
+//           body:JSON.stringify(body)
+//         })
+//         .then(response=>{
+//           console.log("RESPONSE",response)
+//           const {status}=response
+//           console.log("Status",status);
+//         })
+//         .catch(error=>console.log(error))
+//       }
+
+//     return (
+//         <div className="cartitems">
+//            <div className="cartitems-format-main">
+//                 <p>Products</p>
+//                 <p>Title</p>
+//                 <p>Price</p>
+//                 <p>Quantity</p>
+//                 <p>Total</p>
+//                 <p>Remove</p>
+//            </div>
+//            <hr />
+//            {all_product.map((e)=>{
+//             if(cartItems[e.id]>0){
+//                    return  <div>
+//                    <div className="cartitems-format cartitems-format-main">
+//                        <img src={e.image} alt="" className="carticon-product-icon" />
+//                        <p>{e.name}</p>
+//                        <p>${e.new_price}</p>
+//                        <button className="cartitems-quantity">{cartItems[e.id]}</button>
+//                        <p>${e.new_price*cartItems[e.id]}</p>
+//                        <img className="cartitems-remove-icon" src={remove_icon} onClick={()=>{removeFromCart(e.id)}} alt="" />
+//                    </div>
+//               <hr />
+//          </div>
+//             }
+//             return null;
+//            })}
+//            <div className="cartitems-down">
+//             <div className="cartitems-total">
+//                 <h1>Cart Totals</h1>
+//                 <div>
+//                     <div className="cartitems-total-item">
+//                         <p>Subtatal</p>
+//                         <p>${getTotalCartAmount()}</p>
+//                     </div>
+//                     <hr />
+//                     <div className="cartitems-total-item">
+//                         <p>Shipping Fee</p>
+//                         <p>Free</p>
+//                     </div>
+//                     <hr />
+//                     <div className="cartitems-total-item">
+//                         <h3>Total</h3>
+//                         <h3>${getTotalCartAmount()}</h3>
+//                     </div>
+//                 </div>
+//                 {/* <button >PROCEED TO PAYMENT</button> */}
+//                 <StripeCheckout 
+//                  stripeKey={apiKey}
+//                  token={makePayment}
+//                  name="Complete Payment"
+//                  amount={getTotalCartAmount}
+//                  shippingAddress
+//                  billingAddress
+//                  >
+//                    <button className='btn-large blue'>PROCEED TO PAYMENT</button>
+//                  </StripeCheckout>
+//             </div>
+//             <div className="cartitems-promocode">
+//                 <p>If you have any promo code, Enter it here</p>
+//                 <div className="cartitems-promobox">
+//                     <input type="text"placeholder="promo code" />
+//                     <button>Submit</button>
+//                 </div>
+//             </div>
+//            </div>
+//         </div>
+//     )
+// }
+// export default CartItems
+import React, { useContext } from "react";
 import  './CartItems.css'
 import { ShopContext } from "../../Context/ShopContext";
 import remove_icon from '../Assets/cart_cross_icon.png'
-//import {loadStripe} from '@stripe/stripe-js';//for payment intregation
-import StripeCheckout from 'react-stripe-checkout'
-//const apiKey="pk_test_51OpVHpSIyGZ3BZDjKKoAKQ8IZJbLm3xLxkUrMtkuvx4LCCKTcuXeNS42JWaVkgn3RJSJQAlHP8C0x9uD52PODDg500picRPBvt"
-const apiKey = import.meta.env.VITE_API_KEY;
+
 const CartItems= ()=>{
     const {getTotalCartAmount,all_product,cartItems,removeFromCart}=useContext(ShopContext);
-    const [Product,setProduct] = useState({
-        name:{cartItems},
-        price:10,
-        productBy:"facebook"
-      })
-      
-    //for payment intregation
-    const makePayment = token=>{
-        const body={
-          token,
-          Product
-        }
-        const headers={
-          "Content-Type":"application/json"
-        }
-    
-        // return fetch(`http://localhost:3000/payment`,{
-        return fetch(`https://backend3-j9x6.onrender.com/payment`,{
-          method:"POST",
-          headers,
-          body:JSON.stringify(body)
-        })
-        .then(response=>{
-          console.log("RESPONSE",response)
-          const {status}=response
-          console.log("Status",status);
-        })
-        .catch(error=>console.log(error))
-      }
-
     return (
         <div className="cartitems">
            <div className="cartitems-format-main">
@@ -51,7 +128,7 @@ const CartItems= ()=>{
            <hr />
            {all_product.map((e)=>{
             if(cartItems[e.id]>0){
-                   return  <div>
+                   return  <div key={e.id}>
                    <div className="cartitems-format cartitems-format-main">
                        <img src={e.image} alt="" className="carticon-product-icon" />
                        <p>{e.name}</p>
@@ -84,17 +161,7 @@ const CartItems= ()=>{
                         <h3>${getTotalCartAmount()}</h3>
                     </div>
                 </div>
-                {/* <button >PROCEED TO PAYMENT</button> */}
-                <StripeCheckout 
-                 stripeKey={apiKey}
-                 token={makePayment}
-                 name="Complete Payment"
-                 amount={getTotalCartAmount}
-                 shippingAddress
-                 billingAddress
-                 >
-                   <button className='btn-large blue'>PROCEED TO PAYMENT</button>
-                 </StripeCheckout>
+                <button>PROCEED TO PAYMENT</button>
             </div>
             <div className="cartitems-promocode">
                 <p>If you have any promo code, Enter it here</p>
--- frontend/src/Context/ShopContext.jsx
+++ frontend/src/Context/ShopContext.jsx
@@ -1,5 +1,117 @@
+// import React, { createContext, useEffect, useState } from "react";
+// //import all_product from "../Components/Assets/all_product"
+
+// export const ShopContext = createContext(null);
+
+// const getDefaultCart = ()=>{
+//     let cart={};
+//     for(let index=0;index<300;index++){
+//         cart[index]=0;
+//     }
+//     return cart;
+// }
+
+// const ShopContextProvider = (props) =>{
+        
+//       //const contextValue={all_product};
+
+//         const [all_product,setAll_Product] = useState([]);
+//         const [cartItems,setCartItems]=useState(getDefaultCart());
+        
+//         useEffect(()=>{
+//             // fetch('http://localhost:4000/allproducts')
+//             fetch('https://backend3-j9x6.onrender.com/allproducts')
+//             .then((response)=>response.json())
+//             .then((data)=>setAll_Product(data))
+
+//             if(localStorage.getItem('auth-token')){
+//                 // fetch('http://localhost:4000/getcart',{
+//                fetch('https://backend3-j9x6.onrender.com/getcart',{
+//                     method:'POST',
+//                     headers:{
+//                         Accept:'application/form-data',
+//                         'auth-token':`${localStorage.getItem('auth-token')}`,
+//                         'Content-Type':'application/json',
+//                     },
+//                     body:"",
+//                 }).then((response)=>response.json())
+//                 .then((data)=>setCartItems(data));
+//             }
+//         },[])
+
+//         const addToCart=(itemId)=>{
+//             setCartItems((prev)=>({...prev,[itemId]:prev[itemId]+1}))
+//             if(localStorage.getItem('auth-token')){
+//                 // fetch('http://localhost:4000/addtocart',{
+//                 fetch('https://backend3-j9x6.onrender.com/addtocart',{
+//                     method:'POST',
+//                     headers:{
+//                         Accept:'application/form-data',
+//                         'auth-token':`${localStorage.getItem('auth-token')}`,
+//                         'Content-Type':'application/json',
+//                     },
+//                     body:JSON.stringify({"itemId":itemId}),
+//                 })
+//                 .then((response)=>response.json())
+//                 .then((data)=>console.log(data));
+//             }
+//         }
+
+//         const removeFromCart=(itemId)=>{
+//             setCartItems((prev)=>({...prev,[itemId]:prev[itemId]-1}))
+//             if(localStorage.getItem('auth-token')){
+//                  //fetch('http://localhost:4000/removefromcart',{
+//                 fetch('https://backend3-j9x6.onrender.com/removefromcart',{
+//                     method:'POST',
+//                     headers:{
+//                         Accept:'application/form-data',
+//                         'auth-token':`${localStorage.getItem('auth-token')}`,
+//                         'Content-Type':'application/json',
+//                     },
+//                     body:JSON.stringify({"itemId":itemId}),
+//                 })
+//                 .then((response)=>response.json())
+//                 .then((data)=>console.log(data));
+//             }
+//         }
+        
+//         //created logic of getTotalCartAmount in th cart
+//         const getTotalCartAmount = ()=>{
+//             let totalAmount=0;
+//             for(const item in cartItems){
+//                 if(cartItems[item]>0){
+//                     let itemInfo=all_product.find((product)=>product.id===Number(item));
+//                     totalAmount+=itemInfo.new_price*cartItems[item]
+//                 }
+//             }
+//             return totalAmount;
+//         }
+        
+//         //created logic of total items in th cart
+//         const getTotalCartItems = ()=>{
+//             let totalItem=0;
+//             for (const item in cartItems){
+//                 if(cartItems[item]>0){
+//                     totalItem+=cartItems[item];
+//                 }
+//                 console.log(cartItems[item])
+//             }
+//             return totalItem;
+//         }
+
+//         //sending value of throw context api
+//         const contextValue = {getTotalCartItems,getTotalCartAmount,all_product,cartItems,addToCart,removeFromCart};
+
+//     return (
+//         <ShopContext.Provider value={contextValue}>
+//             {props.children}
+//         </ShopContext.Provider>
+//     )
+// }
+// export default ShopContextProvider;
+
 import React, { createContext, useEffect, useState } from "react";
-//import all_product from "../Components/Assets/all_product"
+// import all_product from "../Components/Assets/all_product"
 
 export const ShopContext = createContext(null);
 
@@ -12,21 +124,19 @@ const getDefaultCart = ()=>{
 }
 
 const ShopContextProvider = (props) =>{
-        
-      //const contextValue={all_product};
 
         const [all_product,setAll_Product] = useState([]);
         const [cartItems,setCartItems]=useState(getDefaultCart());
         
         useEffect(()=>{
-            // fetch('http://localhost:4000/allproducts')
-            fetch('https://backend3-j9x6.onrender.com/allproducts')
+             fetch('http://localhost:4000/allproducts')
+            //fetch('https://backend3-j9x6.onrender.com/allproducts')
             .then((response)=>response.json())
             .then((data)=>setAll_Product(data))
 
             if(localStorage.getItem('auth-token')){
-                // fetch('http://localhost:4000/getcart',{
-               fetch('https://backend3-j9x6.onrender.com/getcart',{
+               fetch('http://localhost:4000/getcart',{
+               // fetch('https://backend3-j9x6.onrender.com/getcart',{
                     method:'POST',
                     headers:{
                         Accept:'application/form-data',
@@ -42,8 +152,8 @@ const ShopContextProvider = (props) =>{
         const addToCart=(itemId)=>{
             setCartItems((prev)=>({...prev,[itemId]:prev[itemId]+1}))
             if(localStorage.getItem('auth-token')){
-                // fetch('http://localhost:4000/addtocart',{
-                fetch('https://backend3-j9x6.onrender.com/addtocart',{
+                 fetch('http://localhost:4000/addtocart',{
+                //fetch('https://backend3-j9x6.onrender.com/addtocart',{
                     method:'POST',
                     headers:{
                         Accept:'application/form-data',
@@ -60,8 +170,8 @@ const ShopContextProvider = (props) =>{
         const removeFromCart=(itemId)=>{
             setCartItems((prev)=>({...prev,[itemId]:prev[itemId]-1}))
             if(localStorage.getItem('auth-token')){
-                 //fetch('http://localhost:4000/removefromcart',{
-                fetch('https://backend3-j9x6.onrender.com/removefromcart',{
+                 fetch('http://localhost:4000/removefromcart',{
+                //fetch('https://backend3-j9x6.onrender.com/removefromcart',{
                     method:'POST',
                     headers:{
                         Accept:'application/form-data',
